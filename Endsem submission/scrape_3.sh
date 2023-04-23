c2f2edb55d05dff83a7d517de0b0cbc22e30668b
Bhavya Jain <89458184+bhavya20428@users.noreply.github.com>
2023-04-24 00:34:30 +0530

    Bhavya Final commit

---
+++ Endsem submission/scrape_3.sh
@@ -0,0 +1,15 @@
+instaloader --no-videos
+
+instaloader "samuel.elkins" "laurasykora" "natalieallenco" "darling" "jennakutcher" "sophia_roe" "thebirdspapaya" --no-videos 
+instaloader "emmasedition" "thebalancedblonde" "rclayton" "doyoutravel" "kristinjohns" "victoriadariano" "laurabeverlin" --no-videos
+instaloader "happilygrey" "mimithor" "marianna_hewitt" "gypsea_lust" "jessalizzi" "mrsrodial" "danielwellington" "tanyaburr" --no-videos 
+instaloader "emilyrosehannon" "lucywilliams02" "negin_mirsalehi" "traveltuesday" "fashionedchicstyling" "thegoodtrade" --no-videos
+instaloader "karissaamy" "lizkamarul" "alyson_haley" "zahralyla" "laurencaruso_" "byclairebriston" "angelcandices" "pepamack" --no-videos
+instaloader "thestylevisitor" "charlottejacklin" "juliahengel" "lifewithoutandy" "meccamaxima" "jesskeys_" "meganellaby" "itsmegama" --no-videos
+instaloader "doina" "thegreenhub_" "jayalvarrez" "loveandlemons" "lucylaucht" "oliviapalermo" "kayla_itsines" "leefromamerica" --no-videos
+instaloader "gabbyepstein" "thefashionguitar" "hannahbronfman" "chiaraferragni" "fashionlush" "paolaalberdi" "ashleytisdale" --no-videos
+instaloader "taramilktea" "margoandme" "sincerelyjules" "camillecharriere" "candicebrathwaite" "gretaeagan" "jennymwalton" --no-videos
+instaloader "asliceopi" "rosielondoner" "annstreetstudio" "laurenelizabeth" "nicolettemason" "krista.horton" "natalieoffduty" --no-videos
+instaloader "amy_sall" "wethepeoplestyle" "champagneandchanel" "alwaysjudging" "juliahartx" "tezza" "harperandharley" "troprouge" --no-videos
+instaloader "joellehyman" "vbiancav" "emmahill" "sincerleyjules" "rosannadavison" "thestylestalkercom" "modedamour" "aimeesong" --no-videos
+instaloader "negin_mirsalehi" "marianna_hewitt" "chrissyteigen" "cupofjo" "noelledowning" "beigerenegade " "bychari" "fashioninflux" "emmazoeyroch" --no-videos
